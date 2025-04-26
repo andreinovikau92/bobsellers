0713e11fec19135942c2d8a011ec5c88e51cc381
Andrei Novikau <76206465+andreinovikau92@users.noreply.github.com>
2025-04-26 14:01:59 -0500

    add: a new test Verify the Create an ad! button is visible

--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -1,12 +1,19 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import HomePage from '../page_objects/homePage';
 
 test.describe('homePage.spec.js', () => {
 
-  test('Verify the URL is correct', async ({ page }) => {
-    await page.goto('/');
+  test.beforeEach(async ({page}) => {
+    const homePage = new HomePage(page);
+    await homePage.open();
+  });
+
+  test('Verify the Create an ad! button is visible', async ({ page }) => {
+
+    const homePage = new HomePage(page);
   
-    await expect(page).toHaveURL('https://www.bobseller.com/');
+    await expect(homePage.createAdBtn).toHaveText('Create an ad!');
   });
 
 });
