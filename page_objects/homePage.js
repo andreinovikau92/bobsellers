b5a07f76728d4386eb9c853ce9fb0db4ac6d5aae
Andrei Novikau <andrei.novikau92@gmail.com>
2025-06-11 07:07:28 -0500

    add: Verify a New radio button is checked and Verify a Used radio button is checked

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -7,10 +7,20 @@ class HomePage {
 
         this.createAdBtn = page.locator('.create');
         this.loginInBtn = page.locator('.register[href="/login"]');
+        this.newRadioButton = page.locator('#new');
+        this.usedRadioButton = page.locator('#used');
     }
 
     async open() {
         await this.page.goto('/');
+    }
+
+    async checkNewRadionButton() {
+        await this.newRadioButton.check();
+    }
+
+    async checkUsedRadioButton() {
+        await this.usedRadioButton.check();
     }
 }
 
